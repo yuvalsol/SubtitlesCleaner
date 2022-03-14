b3f9d8b68aeaa8360eae8c241ae21aa608aba6cb
yuvalsol <yuvalsol@gmail.com>
2022-03-14 18:15:10 +0200

    FindAndReplace

---
+++ SubtitlesCL/FindAndReplace.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SubtitlesCL
+{
+    class FindAndReplace
+    {
+        public string Pattern { get; private set; }
+        public string GroupName { get; private set; }
+        public string Replacement { get; private set; }
+        public bool IgnoreCase { get; private set; }
+        public Regex Regex { get; private set; }
+
+        public FindAndReplace(string pattern, string replacement, bool ignoreCase = false)
+        {
+            Pattern = pattern;
+            Replacement = replacement;
+            IgnoreCase = ignoreCase;
+            Regex = new Regex(Pattern, IgnoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
+        }
+
+        public FindAndReplace(string pattern, string groupName, string replacement, bool ignoreCase = false) : this(pattern, replacement, ignoreCase)
+        {
+            GroupName = groupName;
+        }
+
+        public override string ToString()
+        {
+            return
+                Pattern +
+                " -> " +
+                (Replacement.StartsWith(" ") || Replacement.EndsWith(" ") ? "\"" + Replacement + "\"" : Replacement);
+        }
+
+        public string CleanSubtitleLine(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+                return null;
+            else if (string.IsNullOrEmpty(GroupName))
+                return Regex.Replace(line, Replacement);
+            else
+                return Regex.ReplaceGroup(line, GroupName, Replacement);
+        }
+    }
+}
---
+++ SubtitlesCL/RegexExtensions.cs
@@ -0,0 +1,71 @@
+﻿namespace System.Text.RegularExpressions
+{
+    public static partial class RegexExtensions
+    {
+        public static string ReplaceGroup(this Regex regex, string input, string groupName, string replacement)
+        {
+            return regex.Replace(
+                input,
+                match =>
+                {
+                    Group group = match.Groups[groupName];
+                    if (group.Success)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        int previousCaptureEnd = 0;
+                        foreach (Capture capture in group.Captures)
+                        {
+                            int currentCaptureEnd = capture.Index + capture.Length - match.Index;
+                            int currentCaptureLength = capture.Index - match.Index - previousCaptureEnd;
+                            sb.Append(match.Value.Substring(previousCaptureEnd, currentCaptureLength));
+                            sb.Append(replacement);
+                            previousCaptureEnd = currentCaptureEnd;
+                        }
+
+                        sb.Append(match.Value.Substring(previousCaptureEnd));
+
+                        return sb.ToString();
+                    }
+                    else
+                    {
+                        return match.Value;
+                    }
+                }
+            );
+        }
+
+        public static string ReplaceGroup(this Regex regex, string input, int groupNum, string replacement)
+        {
+            return regex.Replace(
+                input,
+                match =>
+                {
+                    Group group = match.Groups[groupNum];
+                    if (group.Success)
+                    {
+                        StringBuilder sb = new StringBuilder();
+
+                        int previousCaptureEnd = 0;
+                        foreach (Capture capture in group.Captures)
+                        {
+                            int currentCaptureEnd = capture.Index + capture.Length - match.Index;
+                            int currentCaptureLength = capture.Index - match.Index - previousCaptureEnd;
+                            sb.Append(match.Value.Substring(previousCaptureEnd, currentCaptureLength));
+                            sb.Append(replacement);
+                            previousCaptureEnd = currentCaptureEnd;
+                        }
+
+                        sb.Append(match.Value.Substring(previousCaptureEnd));
+
+                        return sb.ToString();
+                    }
+                    else
+                    {
+                        return match.Value;
+                    }
+                }
+            );
+        }
+    }
+}
