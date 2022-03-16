525d434ab278d36f4a9f1ee1a69a09f63354df96
yuvalsol <yuvalsol@gmail.com>
2022-03-16 13:21:28 +0200

    changes to FindAndReplace

--- SubtitlesCL/FindAndReplace.cs
+++ SubtitlesCL/FindAndReplace.cs
@@ -5,49 +5,12 @@ namespace SubtitlesCL
 {
     public class FindAndReplace
     {
-        public string Pattern { get; private set; }
+        public Regex Regex { get; private set; }
         public string GroupName { get; private set; }
         public string Replacement { get; private set; }
-        public bool IgnoreCase { get; private set; }
         public SubtitleError SubtitleError { get; private set; }
-        public Regex Regex { get; private set; }
 
         #region Constructors
-
-        public FindAndReplace(string pattern, string replacement, SubtitleError subtitleError)
-            : this(pattern, null, replacement, false, subtitleError)
-        {
-        }
-
-        public FindAndReplace(string pattern, string replacement, bool ignoreCase, SubtitleError subtitleError)
-            : this(pattern, null, replacement, ignoreCase, subtitleError)
-        {
-        }
-
-        public FindAndReplace(string pattern, string groupName, string replacement, SubtitleError subtitleError)
-            : this(pattern, groupName, replacement, false, subtitleError)
-        {
-        }
-
-        public FindAndReplace(string pattern, string groupName, string replacement, bool ignoreCase, SubtitleError subtitleError)
-        {
-            Pattern = pattern;
-            GroupName = groupName;
-            Replacement = replacement;
-            IgnoreCase = ignoreCase;
-            SubtitleError = subtitleError;
-
-            try
-            {
-                Regex = new Regex(Pattern, IgnoreCase ? RegexOptions.IgnoreCase : RegexOptions.None);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(this.ToString());
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-        }
 
         public FindAndReplace(Regex regex, string replacement, SubtitleError subtitleError)
             : this(regex, null, replacement, subtitleError)
@@ -57,10 +20,8 @@ namespace SubtitlesCL
         public FindAndReplace(Regex regex, string groupName, string replacement, SubtitleError subtitleError)
         {
             Regex = regex;
-            Pattern = Regex.ToString();
             GroupName = groupName;
             Replacement = replacement;
-            IgnoreCase = Regex.Options.HasFlag(RegexOptions.IgnoreCase);
             SubtitleError = subtitleError;
         }
 
@@ -68,32 +29,12 @@ namespace SubtitlesCL
 
         #region Case Insensitive
 
-        public bool HasRegexCI { get; private set; }
-        public string PatternCI { get; private set; }
         public Regex RegexCI { get; private set; }
-
-        public FindAndReplace SetRegexCI(string patternCI)
-        {
-            try
-            {
-                RegexCI = new Regex(PatternCI);
-                PatternCI = RegexCI.ToString();
-                HasRegexCI = true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(this.ToStringCI());
-                Console.WriteLine(ex.Message);
-                throw;
-            }
-
-            return this;
-        }
+        private bool HasRegexCI;
 
         public FindAndReplace SetRegexCI(Regex regexCI)
         {
             RegexCI = regexCI;
-            PatternCI = RegexCI.ToString();
             HasRegexCI = true;
             return this;
         }
@@ -112,10 +53,10 @@ namespace SubtitlesCL
             return ToString(true);
         }
 
-        private string ToString(bool isCaseInsensitive)
+        private string ToString(bool cleanHICaseInsensitive)
         {
             return
-                (isCaseInsensitive && HasRegexCI ? PatternCI : Pattern) + " -> " +
+                (cleanHICaseInsensitive && HasRegexCI ? RegexCI : Regex).ToString() + " -> " +
                 (string.IsNullOrEmpty(GroupName) ? string.Empty : GroupName + " -> ") +
                 (Replacement.StartsWith(" ") || Replacement.EndsWith(" ") ? "\"" + Replacement + "\"" : Replacement);
         }
