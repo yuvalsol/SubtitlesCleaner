198f3ac4e0c606f6bb50cd05367479583c7063a0
yuvalsol <yuvalsol@gmail.com>
2023-01-07 08:31:23 +0200

    print formatted stack trace of unhandled exception

---
+++ SubtitlesCleanerEditor/ExceptionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace System
+{
+    public static partial class ExceptionExtensions
+    {
+        public static string GetFormattedStackTrace(this Exception ex)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool isFoundNamespaceNotSystemWindowsForms = false;
+
+            StackTrace st = new StackTrace(ex, true);
+            for (int i = 0; i < st.FrameCount; i++)
+            {
+                StackFrame sf = st.GetFrame(i);
+                if (sf != null)
+                {
+                    MethodBase method = sf.GetMethod();
+                    if (method != null)
+                    {
+                        Type reflectedType = method.ReflectedType;
+                        if (reflectedType != null)
+                        {
+                            if (reflectedType.Namespace != "System.Windows.Forms")
+                            {
+                                isFoundNamespaceNotSystemWindowsForms = true;
+
+                                MethodInfo mi = method as MethodInfo;
+                                if (mi != null)
+                                {
+                                    sb.Append(mi.GetSignature());
+
+                                    int lineNumber = sf.GetFileLineNumber();
+                                    if (lineNumber > 0)
+                                    {
+                                        sb.Append(" at line ");
+                                        sb.Append(lineNumber);
+                                    }
+
+                                    sb.Append("\n");
+                                }
+                            }
+                            else if (isFoundNamespaceNotSystemWindowsForms)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- SubtitlesCleanerEditor/Program.cs
+++ SubtitlesCleanerEditor/Program.cs
@@ -34,7 +34,7 @@ namespace SubtitlesCleanerEditor
                 MessageBox.Show(string.Format("An unhandled error occurred.{0}The application will terminate now.{0}{0}{1}{0}{0}{2}",
                     "\n",
                     ex.Message,
-                    ex.StackTrace.Split('\n')[0]
+                    ex.GetFormattedStackTrace()
                 ), string.Format("Unhandled Error - {0} {1}",
                     Assembly.GetExecutingAssembly().GetName().Name,
                     Assembly.GetExecutingAssembly().GetName().Version.ToString(3)
