f331a1782fd8b5c9bb918da694a87faae9597ed7
yuvalsol <yuvalsol@gmail.com>
2023-01-29 23:54:16 +0200

    more chnages to Merged Words errors

--- SubtitlesCleanerLibrary/SubtitlesHelper.cs
+++ SubtitlesCleanerLibrary/SubtitlesHelper.cs
@@ -3494,10 +3494,8 @@ namespace SubtitlesCleanerLibrary
         #region Merged Words
 
         public static readonly FindAndReplace[] MergedWords = new FindAndReplace[] {
-             new FindAndReplace(new Regex(@"\b(?<OCR>of)(?i:the|you|we|him|her|it|this|they|them|those|thing|things|too)\b", RegexOptions.Compiled), "OCR", "of ", SubtitleError.Merged_Words_Error)
-            ,new FindAndReplace(new Regex(@"\b(?<OCR>on)(?i:the|you|we|him|her|it|this|they|them|those|thing|things|too)\b", RegexOptions.Compiled), "OCR", "on ", SubtitleError.Merged_Words_Error)
-            ,new FindAndReplace(new Regex(@"\b(?<OCR>if)(?i:the|you|we|him|her|it|this|they|them|those|thing|things|too)\b", RegexOptions.Compiled), "OCR", "if ", SubtitleError.Merged_Words_Error)
-            ,new FindAndReplace(new Regex(@"\b(?<Prefix>of|on|if)(?<Suffix>[A-ZÀ-Ý][a-zà-ÿ])", RegexOptions.Compiled), "${Prefix} ${Suffix}", SubtitleError.Merged_Words_Error)
+            new FindAndReplace(new Regex(@"\b(?<Prefix>of|on|if|in)(?i)(?<Suffix>the|you|we|him|her|it|this|they|them|those|thing|things|too)\b", RegexOptions.Compiled), "${Prefix} ${Suffix}", SubtitleError.Merged_Words_Error)
+            ,new FindAndReplace(new Regex(@"\b(?<Prefix>of|on|if|in)(?<Suffix>[A-ZÀ-Ý][a-zà-ÿ])", RegexOptions.Compiled), "${Prefix} ${Suffix}", SubtitleError.Merged_Words_Error)
             ,new FindAndReplace(new Regex(@"\b(?i:after|don't|for|of|our|that|this)(?<OCR>j)", RegexOptions.Compiled), "OCR", " j", SubtitleError.Merged_Words_Error)
             ,new FindAndReplace(new Regex(@"\b(?<OCR>off)(?i:first|too)\b", RegexOptions.Compiled), "OCR", "off ", SubtitleError.Merged_Words_Error)
             ,new FindAndReplace(new Regex(@"\b(?<OCR>fora)\b", RegexOptions.Compiled), "OCR", "for a", SubtitleError.Merged_Words_Error)
